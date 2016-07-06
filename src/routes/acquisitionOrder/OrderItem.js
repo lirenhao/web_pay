e96e8b28edec7eb64d453660d7550fac27358afd
YuKaiyang <45544205@qq.com>
2016-07-06 14:38:09 +0800

    1.加上一个验证的样式

--- src/routes/acquisitionOrder/OrderItem.js
+++ src/routes/acquisitionOrder/OrderItem.js
@@ -22,53 +22,55 @@ var OrderItem = React.createClass({
 	render: function () {
 		return (
 			<Form inline className={s.mgt}>
-				<FormGroup controlId="productName"
-									 validationState={this.getValidationName()}
-				>
-					<InputGroup>
-						<InputGroup.Addon className={s.noBLR}>
-							<Glyphicon glyph="shopping-cart"/>
-						</InputGroup.Addon>
-						<ControlLabel className="sr-only ">productName</ControlLabel>
-						<FormControl className={s.inputRadius} name="items.name"
-												 value={this.props.name}
-												 onChange={this.props.nameChange}
-												 placeholder="商品名称"/>
-						<FormControl.Feedback/>
+				<FormGroup>
+					<FormGroup controlId="productName"
+										 validationState={this.getValidationName()}
+					>
+						<InputGroup>
+							<InputGroup.Addon className={s.noBLR}>
+								<Glyphicon glyph="shopping-cart"/>
+							</InputGroup.Addon>
+							<ControlLabel className="sr-only ">productName</ControlLabel>
+							<FormControl className={s.inputRadius} name="items.name"
+													 value={this.props.name}
+													 onChange={this.props.nameChange}
+													 placeholder="商品名称"/>
+							<FormControl.Feedback/>
+						</InputGroup>
+					</FormGroup>
+					<FormGroup controlId="productPrice"
+										 validationState={this.getValidationPrice()}>
+						<InputGroup className={s.top1}>
+							<InputGroup.Addon className={s.noALR + " " + s.inputRadius}>
+								<Glyphicon glyph="yen"/>
+							</InputGroup.Addon>
+							<ControlLabel className="sr-only ">productPrice</ControlLabel>
+							<FormControl className={s.inputRadius} name="items.price"
+													 value={this.props.price}
+													 onChange={this.props.priceChange}
+													 placeholder="单价"/>
+							<FormControl.Feedback/>
+						</InputGroup>
+					</FormGroup>
+					<FormGroup controlId="productCounts"
+										 validationState={this.getValidationPrice()}>
+						<InputGroup className={s.top2}>
+							<InputGroup.Addon className={s.noALR + " " + s.inputRadius}>
+								<Glyphicon glyph="tags"/>
+							</InputGroup.Addon>
+							<ControlLabel className="sr-only ">productCounts</ControlLabel>
+							<FormControl className={s.noRightBorderR} name="items.quantity"
+													 value={this.props.quantity}
+													 onChange={this.props.quantityChange}
+													 placeholder="数量"/>
+							<FormControl.Feedback/>
+						</InputGroup>
+					</FormGroup>
+					<InputGroup className={s.divRemove}>
+						<Button className={s.btnRemove}
+										onClick={this.props.deleteItem}>删除</Button>
 					</InputGroup>
 				</FormGroup>
-				<FormGroup controlId="productPrice"
-									 validationState={this.getValidationPrice()}>
-					<InputGroup className={s.top1}>
-						<InputGroup.Addon className={s.noALR + " " + s.inputRadius}>
-							<Glyphicon glyph="yen"/>
-						</InputGroup.Addon>
-						<ControlLabel className="sr-only ">productPrice</ControlLabel>
-						<FormControl className={s.inputRadius} name="items.price"
-												 value={this.props.price}
-												 onChange={this.props.priceChange}
-												 placeholder="单价"/>
-						<FormControl.Feedback/>
-					</InputGroup>
-				</FormGroup>
-				<FormGroup controlId="productCounts"
-									 validationState={this.getValidationPrice()}>
-					<InputGroup className={s.top2}>
-						<InputGroup.Addon className={s.noALR + " " + s.inputRadius}>
-							<Glyphicon glyph="tags"/>
-						</InputGroup.Addon>
-						<ControlLabel className="sr-only ">productCounts</ControlLabel>
-						<FormControl className={s.noRightBorderR} name="items.quantity"
-												 value={this.props.quantity}
-												 onChange={this.props.quantityChange}
-												 placeholder="数量"/>
-						<FormControl.Feedback/>
-					</InputGroup>
-				</FormGroup>
-				<InputGroup className={s.divRemove}>
-					<Button className={s.btnRemove}
-									onClick={this.props.deleteItem}>删除</Button>
-				</InputGroup>
 			</Form>
 		)
 	}
