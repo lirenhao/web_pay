2d5951634bb035eaf6a4c1a32fe366f87583bba9
kongli860329@163.com <kongli860329@163.com>
2016-07-13 14:05:04 +0800

    通过LESS方式给BOOTSTRAP中的变量重新赋值

--- src/client.js
+++ src/client.js
@@ -11,7 +11,7 @@ import {match} from "universal-router";
 import routes from "./routes";
 import history from './core/history';
 import {addEventListener} from './core/DOMUtils';
-import 'bootstrap/less/bootstrap.less';
+import './bootstrap.less';
 
 import jQuery from 'jquery';
 
